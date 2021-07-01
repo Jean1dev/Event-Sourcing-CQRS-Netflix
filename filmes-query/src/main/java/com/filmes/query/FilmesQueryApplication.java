505256458179280d0ee4d11c7a13b648d3024fa9
Jean <jeanlucafp@gmail.com>
2021-07-01 19:39:16 -0300

    video 4

---
+++ filmes-query/src/main/java/com/filmes/query/FilmesQueryApplication.java
@@ -0,0 +1,13 @@
+package com.filmes.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmesQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmesQueryApplication.class, args);
+	}
+
+}
---
+++ filmes-query/src/main/resources/db/migration/V1.0__create_all_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE public.ator (
+    id VARCHAR(36) NOT NULL,
+    nome VARCHAR NOT NULL,
+    filme_id VARCHAR(36) NOT NULL,
+    CONSTRAINT "PK_ATOR_TABLE" PRIMARY KEY (id)
+);
+
+CREATE TABLE public.genero (
+    id VARCHAR(36) NOT NULL,
+    titulo VARCHAR NOT NULL,
+    filme_id VARCHAR(36) NOT NULL,
+    CONSTRAINT "PK_GENERO_TABLE" PRIMARY KEY (id)
+);
+
+CREATE TABLE public.filme (
+    id VARCHAR(36) NOT NULL,
+    titulo VARCHAR NOT NULL,
+    subtitulo VARCHAR NULL,
+    dataLancamento DATE NOT NULL,
+    duracao INT NULL,
+    isLike BOOL NULL,
+    CONSTRAINT "PK_FILME_TABLE" PRIMARY KEY (id)
+);
+
+ALTER TABLE public.ator ADD CONSTRAINT fk_filme_id_ator FOREIGN KEY (filme_id) REFERENCES filme(id) ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE public.genero ADD CONSTRAINT fk_filme_id_genero FOREIGN KEY (filme_id) REFERENCES filme(id) ON UPDATE CASCADE ON DELETE SET NULL;
